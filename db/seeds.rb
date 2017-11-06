de23ac3ccb4eef01341b579f82c8e55b53a13f55
Raquel <raquel.queiroz@youse.com.br>
2017-11-06 11:37:22 -0200

    Add Position into Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+positions = Position.create([{ name: 'Presidente' }, { name: 'Governador' }, { name: 'Prefeito' }, { name: 'Deputado Federal' }, { name: 'Senador' }, { name: 'Deputado Estadual' }, { name: 'Vereador' }])
