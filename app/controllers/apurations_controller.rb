202c31c0dba4e3936e2fad017eda88d215b1174e
Raquel <raquel.queiroz@youse.com.br>
2017-11-12 20:35:53 -0200

    add null and white votes

--- app/controllers/apurations_controller.rb
+++ app/controllers/apurations_controller.rb
@@ -4,7 +4,6 @@ class ApurationsController < ApplicationController
   def index
   end
 
-
   def count
     response = ''
     created = 0
@@ -19,9 +18,19 @@ class ApurationsController < ApplicationController
 
     @json.first['votes'].each do |vote|
       voter = Voter.where('voters.cpf = ?', vote['cpf']).to_a.first.id
-      candidate = Candidate.where('candidates.number = ?', vote['number']).to_a.first.id
       election = Election.where('elections.code = ?', @json.first['election_code']).to_a.first.id
-      validate_vote = Vote.where('votes.voter_id = ? AND votes.candidate_id = ? AND votes.election_id = ? ', voter, candidate, election).present?
+      if vote['nulo'].present?
+        position = Position.where('positions.name = ?', vote['nulo']).to_a.first.id
+        candidate = Candidate.where('candidates.position_id = ? AND candidates.number = 0', position).to_a.first.id
+        validate_vote = Vote.where('votes.timestamp = ?', vote['timestamp']).present?
+      elsif vote['branco'].present?
+        position = Position.where('positions.name = ?', vote['branco']).to_a.first.id
+        candidate = Candidate.where('candidates.position_id = ? AND candidates.number = 1', position).to_a.first.id
+        validate_vote = Vote.where('votes.timestamp = ?', vote['timestamp']).present?
+      else
+        candidate = Candidate.where('candidates.number = ?', vote['number']).to_a.first.id
+        validate_vote = Vote.where('votes.voter_id = ? AND votes.candidate_id = ? AND votes.election_id = ? ', voter, candidate, election).present?
+      end
 
       unless validate_vote
         @new = Vote.create(uev_id: id, timestamp: vote['timestamp'], voter_id: voter, candidate_id: candidate, election_id: election)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,28 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-positions = Position.create([{ name: 'Presidente' }, { name: 'Governador' }, { name: 'Prefeito' }, { name: 'Deputado Federal' }, { name: 'Senador' }, { name: 'Deputado Estadual' }, { name: 'Vereador' }])
+positions = Position.create([{ name: 'Presidente' }, { name: 'Governador' }, { name: 'Prefeito' }, { name: 'Deputado Federal' }, { name: 'Senador' }, { name: 'Deputado Estadual' }, { name: 'Vereador' }, { name: 'Nulo' }, { name: 'Branco' }])
+
+election = Election.create(code: '2018', init: '13/11/2017 05:00:00', finish: '13/11/2017 18:30:00')
+
+primeira = Uev.create(token: 'primeira', uev_server: 'uev.herokuapp.com/votes')
+quatro = Uev.create(token: 'quatro')
+cinco = Uev.create(token: 'cinco')
+oito = Uev.create(token: 'oito')
+doze = Uev.create(token: 'doze')
+quinze = Uev.create(token: 'quinze')
+
+Uev.all.each do |uev|
+  Position.all.each do |position|
+    Candidate.create(name: 'Nulo', number: 0, position_id: position.id, uev_id: uev.id, election_id: election.id)
+    Candidate.create(name: 'Branco', number: 1, position_id: position.id, uev_id: uev.id, election_id: election.id)
+  end
+
+  Candidate.create(name: 'Francisco Everardo Oliveira Silva', nickname: 'Tiririca', number: 2222, position_id: 4, uev_id: uev.id, election_id: election.id)
+  Candidate.create(name: 'Enéas Ferreira Carneiro', nickname: 'Eneias', number: 5656, position_id: 4, uev_id: uev.id, election_id: election.id)
+  Candidate.create(name: 'Arthur Curry', nickname: 'Aquaman', number: 8819, position_id: 4, uev_id: uev.id, election_id: election.id)
+  Candidate.create(name: 'Lex Luthor', nickname: 'Lex', number: 1000, position_id: 4, uev_id: uev.id, election_id: election.id)
+  Candidate.create(name: 'Diana Prince Rockwell Trevor', nickname: 'WonderWoman', number: 8819, position_id: 4, uev_id: uev.id, election_id: election.id)
+  Candidate.create(name: 'Bruce Wayne', nickname: 'Batman', number: 99, position_id: 1, uev_id: uev.id, election_id: election.id)
+  Candidate.create(name: 'Clark Kent', nickname: 'SuperMan', number: 12, position_id: 1, uev_id: uev.id, election_id: election.id)
+end
